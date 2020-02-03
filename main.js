544287c7c8d57299d39165eacf9da08d9bf02e8c
Dave Longley <dlongley@digitalbazaar.com>
2020-02-03 00:56:13 -0500

    Split `ControllerKey` into `CapabilityAgent` and `KeystoreAgent`.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 /*!
- * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
 export {AsymmetricKey} from './AsymmetricKey.js';
-export {ControllerKey} from './ControllerKey.js';
+export {CapabilityAgent} from './CapabilityAgent.js';
 export {Hmac} from './Hmac.js';
 export {Kek} from './Kek.js';
 export {KeyAgreementKey} from './KeyAgreementKey.js';
+export {KeystoreAgent} from './KeystoreAgent.js';
 export {KmsClient} from './KmsClient.js';
