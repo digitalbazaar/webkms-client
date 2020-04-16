78e52ceb4fd70537783a1168f715d1655c3f270e
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-16 11:23:57 -0400

    Setup CI and coverage workflow.

---
+++ tests/10-api.spec.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+
+const kmsClient = require('..');
+
+describe('webkms-client API', () => {
+  it('should have proper exports', async () => {
+    should.exist(kmsClient);
+    kmsClient.should.have.property('AsymmetricKey');
+    kmsClient.should.have.property('CapabilityAgent');
+    kmsClient.should.have.property('Hmac');
+    kmsClient.should.have.property('Kek');
+    kmsClient.should.have.property('KeyAgreementKey');
+    kmsClient.should.have.property('KeystoreAgent');
+    kmsClient.should.have.property('KmsClient');
+  });
+});
---
+++ tests/test-karma.js
@@ -0,0 +1,8 @@
+/**
+ * Karma test support.
+ *
+ * Copyright (c) 2011-2020 Digital Bazaar, Inc. All rights reserved.
+ */
+// load polyfills
+// no polyfills are currently required.
+
