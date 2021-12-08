8b8ed92f3e166c7d625faa46e05bc4165c143995
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-12-08 15:44:12 -0500

    Fix Typo.

--- KeystoreAgent.js
+++ KeystoreAgent.js
@@ -11,7 +11,7 @@ import {RECOMMENDED_KEYS} from './recommendedKeys.js';
 const VERSIONS = ['recommended', 'fips'];
 
 /**
- * @class CapabilityAgent
+ * @class KeystoreAgent
  */
 
 export class KeystoreAgent {
